2005241927d60b6f87e401ff133d0073848f7b9e
Yoyung <Yoyung1995@sina.com>
2019-06-14 22:46:07 +0800

    1.6
    18 GPIO Control JiDianQi is OK
    Left GPIO PA11 & PA12 Have Problem.

--- STM32F103C8T6_Code/USER/W5500.c
+++ STM32F103C8T6_Code/USER/W5500.c
@@ -70,6 +70,9 @@ void Yoyung_GPIO_Init(void)
 {
 	GPIO_InitTypeDef  GPIO_InitStructure;	
 
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);    //开启AFIO复用功能
+	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE); //关闭JTAG口
+	
 	/* GPIO引脚初始化配置(PA1,2,3,8,9,10,11,12,15) */
 	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_8 
                           |GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12 
@@ -82,8 +85,8 @@ void Yoyung_GPIO_Init(void)
                           |GPIO_Pin_15);
 	
   /*Configure GPIO pins : PB10 PB11 PB12 PB13 
-                           PB14 PB15 PB3 PB4 
-                           PB5 PB6 PB7 */
+                           PB14 PB15 PB3 PB4 	
+                           PB5 PB6 PB7 */			
 	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13 
                           |GPIO_Pin_14|GPIO_Pin_15|GPIO_Pin_3|GPIO_Pin_4 
                           |GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7;
